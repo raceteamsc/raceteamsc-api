c21bf690c5852cc39eb60665b936844583f90a5b
Chrystian Farias <chrystian.farias@outlook.com>
2022-10-05 14:34:27 -0300

    Destroy event

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -211,6 +211,7 @@ class EventsController {
   static async deleteEvent(req, res) {
     const { id } = req.params;
     try {
+      await database.EventsConfirmations.destroy({ where: { event_id: Number(id) } })
       await database.Events.destroy({ where: { id: Number(id) } });
       return res.status(200).json({ mensagem: `evento id ${id} deletado` });
     } catch (error) {
